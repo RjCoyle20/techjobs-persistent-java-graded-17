be4bcd4efaedbb56ae5d9276a0266a013bae7407
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2023-10-30 21:18:23 -0500

    Finished through to End of Task 3. Not getting Job data populated in SQL

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -4,15 +4,19 @@ package org.launchcode.techjobs.persistent.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
-
+@MappedSuperclass
 public abstract class AbstractEntity {
-
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required")
+    @Size(max = 200, message = "Name must be fewer than 200 characters")
     private String name;
 
     public int getId() {
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotBlank(message = "Description is required")
+    @Size(min = 4, max = 255, message = "Description must be between 4 and 255 characters")
+    private String description;
+
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
